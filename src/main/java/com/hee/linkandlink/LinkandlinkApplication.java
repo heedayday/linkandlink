1823d72e03b03b1b879d83a3f55829d14b54537c
heedayday <heeday3001@gmail.com>
2025-02-14 20:01:11 +0900

    file

---
+++ src/main/java/com/hee/linkandlink/LinkandlinkApplication.java
@@ -0,0 +1,13 @@
+package com.hee.linkandlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkandlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkandlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hee/linkandlink/LinkandlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hee.linkandlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkandlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
